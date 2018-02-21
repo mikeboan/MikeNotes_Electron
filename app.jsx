8d9f673392fe87b9eb2c2de0b19c68ae46915f2b
Mike Boan <mikeboan@gmail.com>
2018-02-21 11:06:37 -0500

    Initial commit w/ electron+react setup

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => (
+  <h1>Hello, world.</h1>
+)
+
+ReactDOM.render(<Root></Root>, document.getElementById('react-root'));
